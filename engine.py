2f00c0af44c6186ae382b0f76f58256742badcf7
Андрей Маракулин <51162917+Annndruha@users.noreply.github.com>
2019-07-15 20:37:15 +0500

    Update engine.py

--- engine.py
+++ engine.py
@@ -2,23 +2,22 @@ import datetime
 import random
 import dictionary
 import wikipedia
-
 from pytz import timezone
 wikipedia.set_lang("RU")
 
 def numerals_days(n):
     if ((10<n) and (n<20)):
-        return 'äíåé'
+        return 'дней'
     else:
         n = n%10
         if n==0:
-            return 'äíåé'
+            return 'дней'
         elif n==1:
-            return 'äåíü'
+            return 'день'
         elif ((n>1) and (n<5)):
-            return 'äíÿ'
+            return 'дня'
         elif (n>=5):
-            return 'äíåé'
+            return 'дней'
 
 def date_and_time_now():
     return datetime.datetime.strftime(datetime.datetime.now(timezone('Europe/Moscow')), "%d.%m.%Y %H:%M:%S")
@@ -38,7 +37,7 @@ def sessiya_mesage(user_id):
         f = open('user_list.txt', 'r+')
     except IOError as e:
         result = date_and_time_now()
-        print('Íå óäàëîñü îòêðûòü ôàéë '+result+ '  UTC\n')
+        print('Не удалось открыть файл '+result+ '  UTC\n')
     else:
         for line in f:
             if (line.find(str(user_id))>=0):
@@ -48,29 +47,29 @@ def sessiya_mesage(user_id):
 
 
 
-    #days_from_begin = (today-datetime.date(2019,2,7)).days#Ïðè ðàçíèöû äàò ïîëó÷àåòñÿ îñîáûé òèï, èçâëåêàåì èç íåãî äíè
+    #days_from_begin = (today-datetime.date(2019,2,7)).days#При разницы дат получается особый тип, извлекаем из него дни
 
 
     sessiya_begin = datetime.date(int(sessiya_begin[2]),int(sessiya_begin[1]),int(sessiya_begin[0]))
     days_to_end = (sessiya_begin-today).days
 
     if days_to_end<=-30:
-        return 'Ñåññèÿ óæå ïðîøëà, íàäåþñü òû õîðîøî å¸ ñäàë!'
+        return 'Сессия уже прошла, надеюсь ты хорошо её сдал!'
     elif days_to_end<=0:
-        return 'Ñåññèÿ óæå èä¸ò! Òû ìîëîäåö, ÿ â òåáÿ âåðþ è æåëàþ óñïåõà íà ýêçàìåíàõ! &#10084;'
+        return 'Сессия уже идёт! Ты молодец, я в тебя верю и желаю успеха на экзаменах! &#10084;'
     else:
-        return 'Äî áëèæàéøåãî ýêçàìåíà: '+ str(days_to_end) +' '+ numerals_days(days_to_end)# +'\nÓæå ïðîøëî: '+str(days_from_begin*100//(days_from_begin+days_to_end))+'% ñåìåñòðà'
+        return 'До ближайшего экзамена: '+ str(days_to_end) +' '+ numerals_days(days_to_end)# +'\nУже прошло: '+str(days_from_begin*100//(days_from_begin+days_to_end))+'% семестра'
 
 
 def find_in_wiki(wiki_request):
     try:
-        n=2#Ïî óìëî÷àíèþ âîçâðàùàåì äâà ïðåäëîæåíèÿ
+        n=2#По умлочанию возвращаем два предложения
         exit = 0
 
         while ((n<5) and (exit==0)):
             exit = 1
             ans = str(wikipedia.summary(wiki_request, sentences=n, auto_suggest=True))
-            if ((ans.rfind('('))>(ans.rfind(')'))):#Èùåì êîíåö ïðåäëîæåíèÿ íå ìåæäó ñêîáêàìè
+            if ((ans.rfind('('))>(ans.rfind(')'))):#Ищем конец предложения не между скобками
                 n=n+1
                 exit = 0
             if len(ans)<100:
@@ -91,12 +90,3 @@ def find_in_wiki(wiki_request):
     except wikipedia.exceptions.DisambiguationError:
         ans = dictionary.random_not_found[9]
     return ans
-
-
-
-
-
-
-
-
-
