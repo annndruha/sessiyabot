7f231556161bc8c422aba26e6894dd1304010876
annndruha <a.p.marakulin@gmail.com>
2019-12-04 02:34:20 +0300

    Add day and yearstaday plot

--- core/online_monitor.py
+++ core/online_monitor.py
@@ -1,19 +1,17 @@
 # sessiyabot/core/online_monitor.py
-# Marakulin Andrey
 # Monitor to write down online status of followers
+# Marakulin Andrey @annndruha
 # 2019
+
 import time
 import datetime
 import traceback
 
-from vk_api import VkApi
-from vk_api.keyboard import VkKeyboard
-from vk_api.longpoll import VkLongPoll, VkEventType
 import psycopg2
+import numpy as np
+import matplotlib.pyplot as plt
 
-from data import config
-
-vk = VkApi(token=config.access_token)
+import config ## from data !!!!!!!!!
 
 connection = psycopg2.connect(dbname=config.db_name,
     user=config.db_account,
@@ -29,62 +27,107 @@ def reconnect_db():
     host= config.db_host,
     port = config.db_port)
 
-
-def day_plot(id):
-    '''
-    Dont work well!
-    '''
-    # user = vk.method('users.get', {'user_ids': id})
-
+def yesterday_plot(id):
     with connection.cursor() as cur:
-        cur.execute("SELECT tstamp FROM sessiyabot.online WHERE id=%s", (id,)) # AND status=1;
-        timestams = cur.fetchmany(1440)
+        cur.execute("SELECT tstamp FROM sessiyabot.online WHERE id=%s ORDER BY tstamp DESC LIMIT 2880;", (id,))
+        timestamps = cur.fetchmany(2880)
 
-        cur.execute("SELECT status FROM sessiyabot.online WHERE id=%s", (id,)) # ORDER By
-        status = cur.fetchmany(1440)
-        #connection.commit()
+        cur.execute("SELECT status FROM sessiyabot.online WHERE id=%s ORDER BY tstamp DESC LIMIT 2880;", (id,))
+        status = cur.fetchmany(2880)
 
 
-    print(len(timestams))
-    online = []
-    for i, timestamp in enumerate(timestams):
-        if status[i][0]==1:
-            online.append(timestamp[0])
-    print(len(online))
-
-    #print(min(online))
-    #print(max(online))
-    if len(online)<3:
+    if len(timestamps)<3:
         raise Exception('small data') # Скорей всего вы подписались недавно, эта функция заработает для вас через 10 минут 
 
-    if max(online)-min(online) < datetime.timedelta(minutes= 5):
-        raise Exception('only one hour data') # Вы были в сети не больше пяти минут, поздравляю!
-        
-
     hour_bins = []
-    [hour_bins.append(status.hour) for status in online]
+    current_dat = timestamps[0][0].day
+    for i, timestamp in enumerate(timestamps):
+        if status[i][0]==1:
+            con = ((current_dat==1)and(timestamp[0].day==31)) or ((current_dat==1)and(timestamp[0].day==30)) or ((current_dat==1)and(timestamp[0].day==29)) or ((current_dat==1)and(timestamp[0].day==28))
+            if (timestamp[0].day == current_dat-1) or con:
+                h = timestamp[0].hour
+                hour_bins.append(h)
 
+
+
+    from collections import Counter
+    print(Counter(hour_bins).keys())
+    print(Counter(hour_bins).values())
 
     fig, axs = plt.subplots()
-    
-    
+
+    yesterday = timestamps[0][0] - datetime.timedelta(hours=24)
+    lbl = 'Статистика за '+str(yesterday.day)+'.'+str(yesterday.month)+'.'+str(yesterday.year)
     plt.grid(True, linestyle =":")
     plt.xlabel('Часы')
     plt.ylabel('Минут в сети')
-    lbl = 'Статистика для id= '+ str(id)
 
 
-
-    h = datetime.datetime.now().hour
-    rng = range(h, h-24, -1)
-
-    plt.xticks(rng, size='small')
-    axs.hist(hour_bins, bins = range(min(rng), max(rng)+2), label=lbl, color='#8b00ff')
-    plt.xlim(min(rng), max(rng)+1)
-
+    plt.xticks(ticks=range(0,24), size='small')
+    
+    axs.hist(hour_bins, bins = range(0,25), label=lbl, color='#8b00ff') 
+    plt.xlim([0,24])
 
     axs.legend()
     plt.savefig('./temp.png', dpi=400, bbox_inches='tight')
     plt.show()
 
-    return 0
+    time_online = str(datetime.timedelta(minutes= len(hour_bins)))
+    return 'Вчера вы были онлайн: '+ time_online.split(':')[0]+'ч '+time_online.split(':')[1]+'м'
+
+
+def day_plot(id):
+    with connection.cursor() as cur:
+        cur.execute("SELECT tstamp FROM sessiyabot.online WHERE id=%s ORDER BY tstamp DESC LIMIT 1440;", (id,))
+        timestamps = cur.fetchmany(1440)
+
+        cur.execute("SELECT status FROM sessiyabot.online WHERE id=%s ORDER BY tstamp DESC LIMIT 1440;", (id,))
+        status = cur.fetchmany(1440)
+
+
+    if len(timestamps)<3:
+        raise Exception('small data') # Скорей всего вы подписались недавно, эта функция заработает для вас через 10 минут 
+
+    hour_bins = []
+    last_hour = timestamps[0][0].hour
+    for i, timestamp in enumerate(timestamps):
+        if status[i][0]==1:
+            h = timestamp[0].hour
+            if h>last_hour:
+                hour_bins.append(h-24)
+            else:
+                hour_bins.append(h)
+
+
+    fig, axs = plt.subplots()
+
+    lbl = 'Статистика за последние сутки'
+    plt.grid(True, linestyle =":")
+    plt.xlabel('Часы')
+    plt.ylabel('Минут в сети')
+
+
+    plot_range = range(last_hour-23, last_hour+1)
+    x_lables = []
+    for l in plot_range:
+        if l<0:
+            l+=24
+            x_lables.append(str(l))
+        else:
+            x_lables.append(str(l))
+
+    plt.xticks(ticks=plot_range, labels=x_lables, size='small')
+    
+    axs.hist(hour_bins, bins = range(last_hour-23, last_hour+2), label=lbl, color='#8b00ff') 
+    plt.xlim(last_hour-23, last_hour+1)
+
+    axs.legend()
+    plt.savefig('./temp.png', dpi=400, bbox_inches='tight')
+    plt.show()
+
+    time_online = str(datetime.timedelta(minutes= len(hour_bins)))
+    return 'За последние сутки вы были онлайн: '+ time_online.split(':')[0]+'ч '+time_online.split(':')[1]+'м'
+
+
+id = 478143147
+print(day_plot(id))
